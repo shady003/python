088e02e18d401f5ebd77f422bb498ceb1c9d489c
shady003 <saurabhyadav9909@gmail.com>
2022-11-20 18:22:25 +0530

    fectorial by recursion update

--- factorial.py
+++ factorial.py
@@ -9,4 +9,17 @@ x = int(input('Enter the value for factorial\n>>>'))
 result = fact(x)
 print("factorial of {} is {}".format(x, result))
 
+# by recursion
+
+
+def facto(num):
+    if num == 0:
+        return 1
+    return num * fact(num-1)
+
+
+x = int(input('enter no for fact\n>>>'))
+fac = facto(x)
+print('factorial of {} by recursion method is {}'.format(x, fac) )
+
 
