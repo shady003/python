c26aeb41eb70f577ba2f67d7e30d047312b836e2
shady003 <saurabhyadav9909@gmail.com>
2022-11-20 18:45:46 +0530

    lambda : an anonymous function

--- Anonymousfunc.py
+++ Anonymousfunc.py
@@ -2,3 +2,15 @@
 f = lambda a, b: a + b
 result = f(5, 6)
 print(result)
+
+
+# filter() , map() and reduce()
+
+from functools import reduce
+nums = [1, 32, 3, 4, 5, 6]
+evens = list(filter(lambda n: n % 2 == 0, nums))
+doubles = list(map(lambda n: n * 2, evens))
+sum = reduce(lambda a, b: a+b, doubles)
+print(evens)
+print(doubles)
+print(sum)
