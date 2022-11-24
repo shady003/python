d3cc12b3dfbe059593ef9d2e4148c1e28ce7a71f
shady003 <saurabhyadav9909@gmail.com>
2022-11-24 22:10:54 +0530

    constructor

--- OOPs.py
+++ OOPs.py
@@ -1,18 +1,23 @@
 class Computer:
-
-    def __init__(self, cpu, ram):   # Constructor
+    director = 'Saurabh'
+    def __init__(self, cpu, ram):
         self.cpu = cpu
         self.ram = ram
 
+    # @classmethod
     def config(self):
 
         print(self.cpu, self.ram)
 
 
-com1 = Computer('i5', 6)
-com2 = Computer('Ryzon 5',16)
+com1 = Computer('i5', 6)  # Constructor
+com2 = Computer('Ryzon 5',16)  # Constructor
+com1.ram = 16
 Computer.config(com1)
 com2.config()
+print(com1.director)
 
+print(id(com1))
+print(id(com2))
 a = 100
 print(a.bit_length())
