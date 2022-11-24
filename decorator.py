7ef525eac3bda7df631571adbe529e3de7780136
shady003 <saurabhyadav9909@gmail.com>
2022-11-24 15:45:39 +0530

    decorator_add+2

--- decorator.py
+++ decorator.py
@@ -1,6 +1,11 @@
 def div(a, b):
     return (a/b)
 
+
+def add(a, b):
+    return a+b
+
+
 def smart_div(div):
     def change(a, b):
         if a < b:
@@ -9,5 +14,13 @@ def smart_div(div):
     return change
 
 
+def add2(add):
+    def chng(a, b):
+        b = b + 2
+        return add(a, b)
+    return chng
+
+add = add2(add)
 div = smart_div(div)
 print(div(2, 4))
+print(add(2, 4))
