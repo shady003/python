e49cc2250b49a3769a15077ca6bb167b3ce992ef
shady003 <saurabhyadav9909@gmail.com>
2022-11-25 21:06:37 +0530

    function overloaded

---
+++ operatorOverloading.py
@@ -0,0 +1,17 @@
+class Student :
+    def __init__(self, m1, m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def __add__(self, other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        m3 = Student(m1, m2)
+        return m3
+
+s1 = Student(58, 69)
+s2 = Student(60, 65)
+
+s3 = s1 + s2
+print(s3.m1)
+
